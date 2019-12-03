2e79acec61fa934c5db9d905b02ed0e6c3c6ebf4
TobyBluck <57405593+TobyBluck@users.noreply.github.com>
2019-12-03 16:27:50 +0000

    Add files via upload

---
+++ Booking/Booking/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booking
+{
+    public class bookingclass
+    {
+
+        string name;
+        DateTime whenBook;
+        int room;
+        string staffmember;
+
+        bookingclass book = new bookingclass { 
+        //customer name? 
+        Console.WriteLine("What is your name?");
+
+
+        //when do you want to book?
+        Console.WriteLine("When do you wish to book?");
+
+
+        //what room do you want to book?
+        Console.WriteLine("What room do you wish to book?");
+
+        //staff member taking booking? 
+        Console.WriteLine("Staff Member helping you?");
+        }
+    }
+}
