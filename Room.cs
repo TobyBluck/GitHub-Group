b1cf741605b1c48671bb76eb68a4f25315eb9f44
11hodgecal <57405575+11hodgecal@users.noreply.github.com>
2019-12-03 16:26:43 +0000

    Add files via upload

---
+++ Room.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Room
+    {
+        
+        public Double price;
+
+        public Room(bool free,double price)
+        {
+            this.free = free;
+            this.price = price;
+        }
+
+        public void free()
+        {
+            bool free = true;
+
+            if (free = false)
+
+        }
+        
+        
+    }
+}
